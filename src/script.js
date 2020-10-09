00553b5b47aebb4ba54a3c1da5c090770646c3d2
kandabi <kandabiaviv@gmail.com>
2020-10-09 17:57:35 +0300

    0.3

--- src/script.js
+++ src/script.js
@@ -1,23 +1,6 @@
-
-
-// window.onload = function() {
-//     Particles.init({
-//       selector: '.canvas',
-//       //color: "#FCED16",
-//       color:"#FD1056",
-//       sizeVariations:4,
-//       maxParticles:140,
-//       minDistance: 110,
-//       connectParticles: true
-//     });
-//   };
-
-
-
 $(function () {
     Particles.init({
-        selector: '.canvas',
-        //color: "#FCED16",
+        selector: '#canvas',
         color: "#FD1056",
         sizeVariations: 4,
         maxParticles: 140,
@@ -25,13 +8,56 @@ $(function () {
         connectParticles: true
     });
 
-    $(".letter").hover(function () {
-        console.log("anim")
-        $(this).addClass("animation-target");
-    })
+    $(".sidebar .bigspin, .text-main .bigspin").hover(function () {
+        $(this).addClass("animation-bigspin");
+    });
 
+    $(".sidebar .bigspin, .text-main .bigspin").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
+        $(this).removeClass("animation-bigspin")
+    });
+
+    $(".bounce").hover(function () {
+        console.log('bounce')
+        $(this).addClass("animation-bounce");
+    });
+
+    $(".bounce").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
+        $(this).removeClass("animation-bounce")
+    });
+
+    $(".letter").hover(function () {
+        $(this).addClass("animation-spin");
+    });
 
     $(".letter").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
-        $(this).removeClass("animation-target")
-    })
+        $(this).removeClass("animation-spin")
+    });
+
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        centeredSlides: true,
+        loop: true,
+        autoplay: { delay: 3000, },
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      })
+
+
+      var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        loop: true,
+        smartBackspace:false,
+        typeSpeed: 180,
+        backSpeed: 100,
+        backDelay: 1200,
+        startDelay: 700,
+      });
 });
