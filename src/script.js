507e4d36a4475a41d78ec0c94f5532cf40f522ea
kandabi <kandabiaviv@gmail.com>
2020-10-13 22:20:52 +0300

    0.9

--- src/script.js
+++ src/script.js
@@ -1,60 +1,54 @@
 
 $(function () {
     Init();
-    if(enableMap)
-        include("https://maps.googleapis.com/maps/api/js?key=AIzaSyCrH9vga3QM9PzgYfb_1goHhIGSSos_gQo&callback=initMap&libraries=&v=weekly")
+
+    var scrollPos = $(document).scrollTop();
+    console.log('init scrolltop: ', scrollPos)
+    if(scrollPos)
+        loadMap();
  });
 
+var mapIncluded = false;
+function loadMap()
+{
+    mapIncluded = true;
+    window.onscroll = null;
+    //include("https://maps.googleapis.com/maps/api/js?key=AIzaSyCrH9vga3QM9PzgYfb_1goHhIGSSos_gQo&callback=initMap&libraries=&v=weekly")
+}
 
 function Init()
 {
     console.log("Is Mobile: %s", device.mobile());
-
     particlesJS.load('particles', './src/particles.json');
 
-    $(".sidebar .bigspin, .text-main .bigspin").hover(function () {
-        $(this).addClass("animation-bigspin");
-    });
+    // $(".sidebar .bigspin, .text-main .bigspin").hover(function () {
+    //     $(this).addClass("animation-bigspin");
+    // });
 
-    $(".sidebar .bigspin, .text-main .bigspin").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
-        $(this).removeClass("animation-bigspin")
+    $(".letter").hover(function () {
+        $(this).addClass("animation-spin");
     });
 
     $(".bounce").hover(function () {
-        console.log('bounce')
         $(this).addClass("animation-bounce");
     });
 
-    $(".bounce").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
-        $(this).removeClass("animation-bounce")
-    });
-
-    $(".letter").hover(function () {
-        $(this).addClass("animation-spin");
-    });
-
-    $(".letter").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
-        $(this).removeClass("animation-spin")
-    });
-
-    $(".letter").hover(function () {
-        $(this).addClass("animation-spin");
+    $(".letter, .bounce").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
+        $(this).removeClass("animation-bounce animation-splat")
     });
     
     var mySwiper = new Swiper('.swiper-container', {
-        // Optional parameters
         direction: 'horizontal',
         centeredSlides: true,
-        // loop: true,
-        autoplay: { delay: 3000, },
-        // If we need pagination
         pagination: {
           el: '.swiper-pagination',
+          clickable: true,
         },
       
-        // And if we need scrollbar
         scrollbar: {
           el: '.swiper-scrollbar',
+          draggable: false,
+          clickable: false,
         },
       })
 
@@ -72,7 +66,7 @@ function Init()
 
     $('.sidebar .item, .contact_btn').click(function(){
             var name = $(this).attr('value');
-            $('html, body').animate({
+            $('html, body').stop().animate({
                 scrollTop: $("." + name).offset().top
             }, 1500);
              
@@ -80,21 +74,66 @@ function Init()
 
     $(".form").submit(function(e) {
         e.preventDefault();
-      
-        var $form = $(this);
-        $.post($form.attr("action"), $form.serialize()).then(function() {
-            alert("Thank you!");
+        // $.post("/functions/send-mail", $form.serialize()).then(function() {
+        //     alert("Thank you!");
+        // });
+        // var form = $(this);
+        // $.ajax({
+        //     url: ".netlify/functions/send-email", // url where to submit the request
+        //     type : "POST", // type of action POST || GET
+        //     dataType : 'json', // data type
+        //     data : form.serialize(), // post data 
+        //     success : function(result) {
+        //         console.log(result);
+        //     },
+        //     error: function(text) {
+        //         console.log(text);
+        //     }
+        // })
+        // $.post(, $form.serialize()).then(function(data, status){
+        //     console.log("response: " + data);
+        // });
+    });
+
+    $('.loader').fadeOut(500 ,function(){
+        $('.content, .sidebar').animate({
+            opacity: 1
+        }, 700);
+    });
+
+    var letters = $('.home .text-main .bounce, .home .text-main .logo');
+    var time = 150;
+
+    setTimeout(function(){
+        letters.each(function(index) {
+            setTimeout( function(){ 
+                $(letters[index]).css('opacity',1).addClass("animation-splat");
+            }, time += 130 )
         });
-    });
 
-    $.get("/.netlify/functions/hello", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
+        var items = $('.contact_btn, .home .text-secondary');
+        time += 750;
+        items.each(function(index) {
+            setTimeout( function(){ 
+                $(items[index]).css('opacity',1).addClass("animation-splat");
+            }, time += 150 )
+        });
+    
+    }, 1500);
+
+    window.onscroll = function() {
+        console.log('window.onscroll')
+        if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+            if(!mapIncluded)
+            {
+                loadMap();
+            }
+            else{
+                window.onscroll = null;
+            }
+         }
+    };
 }
-
-
-
-
 
 
  function include(filename)
