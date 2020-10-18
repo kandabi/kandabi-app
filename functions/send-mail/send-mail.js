0d1b7333252484c1f0c0d04d56c901e667d1503c
kandabi <kandabiaviv@gmail.com>
2020-10-18 16:14:24 +0300

    Update send-mail.js

--- functions/send-mail/send-mail.js
+++ functions/send-mail/send-mail.js
@@ -6,33 +6,20 @@ exports.handler = async (event, context) => {
     };
 
     try {
-      response.body = 'try';
-
       const options = {
         user: process.env.EMAIL_ADDRESS,
         pass: process.env.EMAIL_PASS,
         to: 'kandabiaviv@gmail.com',
       };
 
-      response.body = 'options';
       const send = require('gmail-send')(options);
-      response.body = 'require';
 
-      send({ 
-        subject: 'subject' ,
-        text: 'test test', 
-      }, 
-        (error, result, fullResult) => {
-          if (error) {
-            response.statusCode = 500;
-            response.body = error.toString() ;
-          }
-          else {
-            response.body = result;
-          }
+      const { result, full } = await send({ 
+          subject: 'subject' ,
+          text: 'message body', 
       });
 
-      response.body = 'send';
+      response.body = result;
     }
     catch (error) {
         response.statusCode = 500;
