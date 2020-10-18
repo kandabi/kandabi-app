e9d006c73c1eac96b3ba35bd46a4031eff3af6dc
kandabi <kandabiaviv@gmail.com>
2020-10-18 15:12:59 +0300

    Update send-mail.js

--- functions/send-mail/send-mail.js
+++ functions/send-mail/send-mail.js
@@ -1,34 +1,47 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const options = {
-    user: process.env.EMAIL_ADDRESS,
-    pass: process.env.EMAIL_PASS,
-    to: 'kandabiaviv@gmail.com',
-};
 
-return options;
 
-const send = require('gmail-send')(options);
+
+
 
 exports.handler = async event => {
-    var response;
-    send({ 
-      subject: 'subject' ,
-      text: 'test test', 
-    }, 
-      (error, result, fullResult) => {
-        if (error) {
-          return {
-            statusCode: 500,
-            body: error,
+    try 
+    {
+      const options = {
+          user: process.env.EMAIL_ADDRESS,
+          pass: process.env.EMAIL_PASS,
+          to: 'kandabiaviv@gmail.com',
+      };
+      
+      const send = require('gmail-send')(options);
+
+      send({ 
+        subject: 'subject' ,
+        text: 'test test', 
+      }, 
+        (error, result, fullResult) => {
+          if (error) {
+            return {
+              statusCode: 500,
+              body: error,
+            }
           }
-        }
-        else {
-         return {
-            statusCode: 200,
-            body: result,
+          else {
+           return {
+              statusCode: 200,
+              body: result,
+            }
           }
+      });
+    }
+    catch (error) {
+        return { 
+          statusCode: 500, 
+          body: error.toString() 
         }
-    });
+    }
+
+    
     
     return response;
 }
