38ba2040b44107fdeae5308161653765f14351f4
kandabi <kandabiaviv@gmail.com>
2020-10-18 14:53:12 +0300

    1.3
    implemented gmail api lambda function

---
+++ functions/gmail-send/mail.js
@@ -0,0 +1,37 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const options = {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASS,
+    to:   'kandabiaviv@gmail.com',
+};
+
+const send = require('gmail-send')(options);
+
+exports.handler = async event => {
+    var response;
+    send({ 
+      subject: 'subject' ,
+      text: 'test test', 
+    }, 
+      (error, result, fullResult) => {
+        if (error) {
+          response = {
+            statusCode: 500,
+            body: error,
+          }
+        }
+        else {
+          response = {
+            statusCode: 200,
+            body: result,
+          }
+        }
+    });
+    
+    return response;
+}
+
+
+
+
+
